46ee69fb5634ac89c2442d2183912d82ebe0d894
Aadarshab <72687881+Aadarshab@users.noreply.github.com>
2023-02-21 16:49:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Aadarshab/Mdisk-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Aadarshab/Mdisk-Search-Bot-Pro /MessageSearchBot
+cd /MessageSearchBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
